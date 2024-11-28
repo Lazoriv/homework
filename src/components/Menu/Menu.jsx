180ecd04eacc7c6a6547517744f62f2af83537d2
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2024-11-28 16:16:26 +0200

    Фіксація змін із врахуванням перенесення папок і зміни регістру

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,38 @@
+import { useState, useEffect } from 'react';
 import './Menu.css';
-import pizzas from "./data";
 import PizzaItem from "./PizzaItem/PizzaItem";
 import Title from '../Title';
 import Subtitle from '../Subtitle';
 
 const Menu = () => {
+
+    const [items, setItems] = useState([]);
+
+    const getPizzas = async () => {
+        try {
+            const res = await fetch('https://react-fast-pizza-api.onrender.com/api/menu');
+            if (!res.ok) {
+                throw new Error("Failed to fetch");
+            }
+            const data = await res.json();
+            setItems(data.data);
+        } catch (e) {
+            console.log(e.message);
+        }
+
+    };
+
+    useEffect(() => {
+        getPizzas();
+    }, [])
+
+
     return (
         <main>
             <Title text="The best pizza." />
             <Subtitle text="Straight out of the oven, straight to you." />
             <div className="menu-container">
-                {pizzas.map((pizza) => (
+                {items.map((pizza) => (
                     <PizzaItem pizza={pizza} key={pizza.id} />
                 ))}
             </div>
