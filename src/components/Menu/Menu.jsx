443c0828747c904a3cd1b4a23790ef8d3df7e84c
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2024-11-29 08:42:02 +0200

    Фіксація змін із врахуванням перенесення папок і зміни регістру

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,39 +1,36 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Menu.css';
-import PizzaItem from "./PizzaItem/PizzaItem";
+import PizzaItem from './PizzaItem/PizzaItem';
 import Title from '../Title';
 import Subtitle from '../Subtitle';
 
 const Menu = () => {
-
     const [items, setItems] = useState([]);
 
-    const getPizzas = async () => {
-        try {
-            const res = await fetch('https://react-fast-pizza-api.onrender.com/api/menu');
-            if (!res.ok) {
-                throw new Error("Failed to fetch");
-            }
-            const data = await res.json();
-            setItems(data.data);
-        } catch (e) {
-            console.log(e.message);
-        }
-
-    };
-
     useEffect(() => {
-        getPizzas();
-    }, [])
+        const fetchPizzas = async () => {
+            try {
+                const res = await fetch('https://react-fast-pizza-api.onrender.com/api/menu');
+                if (!res.ok) {
+                    throw new Error("Failed to fetch pizzas");
+                }
+                const data = await res.json();
+                setItems(data.data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
 
+        fetchPizzas();
+    }, []);
 
     return (
         <main>
-            <Title text="The best pizza." />
-            <Subtitle text="Straight out of the oven, straight to you." />
             <div className="menu-container">
+                <Title text="The best pizza." />
+                <Subtitle text="Straight out of the oven, straight to you." />
                 {items.map((pizza) => (
-                    <PizzaItem pizza={pizza} key={pizza.id} />
+                    <PizzaItem key={pizza.id} pizza={pizza} />
                 ))}
             </div>
         </main>
