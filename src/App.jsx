91b4bf04a0a8d9cbd19b3446f9701bcf8f1e88eb
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2024-12-06 17:21:20 +0200

    Фіксація змін із врахуванням перенесення папок і зміни регістру

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Header from './components/Header/Header';
 import Main from './pages/Main';
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
 
 const App = () => {
+  const [userName, setUserName] = useState("");
   return (
-    <div>
-      <Header />
-      <Main />
-    </div>
+    <AuthContext.Provider value={{ userName, setUserName }}>
+      <div>
+        <Header />
+        <Main />
+      </div>
+    </AuthContext.Provider>
   );
 };
 
