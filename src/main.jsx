e20c8c26190a10243de10905aa193d7fafaa2581
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2024-12-07 09:14:42 +0200

    Фіксація змін із врахуванням перенесення папок і зміни регістру

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,20 +3,20 @@ import Cart from '../pages/Cart/Cart';
 import Form from '../pages/Form/Form';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from '../context/AuthContext';
+
 
 const Main = () => {
 
     return (
-        <AuthProvider>
-            <main>
-                <Routes>
-                    <Route path="/" element={<Form action="" method="GET" />} />
-                    <Route path="/menu" element={<Menu />} />
-                    <Route path="/cart" element={<Cart />} />
-                </Routes>
-            </main>
-        </AuthProvider>
+
+        <main>
+            <Routes>
+                <Route path="/" element={<Form action="" method="GET" />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </main>
+
     )
 }
 
