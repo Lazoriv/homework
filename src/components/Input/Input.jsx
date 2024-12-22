1ef77f3869fa6c2e14e8fdbd63a4be7c8b96417f
EvgeniyLazoriv <EvgeniyLazoriv@yandex.com>
2024-12-22 15:03:50 +0200

    Фіксація змін із врахуванням перенесення папок і зміни регістру

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,18 +1,31 @@
+import { useController } from 'react-hook-form';
 import './Input.css';
 
 const Input = (props) => {
-    const { placeholder, type, 'aria-label': ariaLabel, className, value, onChange, id, readOnly, required } = props;
-    return <input
-        type={type}
-        id={id}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        aria-label={ariaLabel}
-        className={className}
-        readOnly={readOnly}
-        required={required}
-    />;
+    const {
+        //control, 
+        name,
+        placeholder,
+        type,
+        'aria-label': ariaLabel,
+        className,
+        id
+    } = props;
+
+    // const { field } = useController({ name, control });
+
+    return (
+        <div>
+            <input
+                type={type}
+                id={id}
+                placeholder={placeholder}
+                aria-label={ariaLabel}
+                className={className}
+            // {...field}
+            />
+        </div>
+    );
 };
 
 export default Input;
--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,81 +1,90 @@
 import './Order.css';
-import { useState } from 'react';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
 import Title from '../../components/Title';
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from "zod";
+
+const schema = z.object({
+    firstName: z.string().min(1, { message: "Ім'я є обов'язковим" }),
+    phone: z.string().min(10, { message: 'Номер телефону має бути щонайменше 10 символів' }),
+    address: z.string().min(1, { message: "Адреса поля є обов'язковим" }),
+    priority: z.boolean().optional(),
+});
 
 const Order = () => {
-    const [firstName, setFirstName] = useState('vlad');
-    const [phone, setPhone] = useState('');
-    const [address, setAddress] = useState('');
-    const [priority, setPriority] = useState(false);
+    const form = useForm({
+        mode: 'onBlur',
+        defaultValues: {
+            firstName: '',
+            phone: '',
+            address: '',
+            priority: false,
+        },
+        resolver: zodResolver(schema),
+    });
 
-    const handleFirstNameChange = (e) => {
-        setFirstName(e.target.value);
-    };
-
-    const handlePhoneChange = (e) => {
-        setPhone(e.target.value);
-    };
-
-    const handleAddressChange = (e) => {
-        setAddress(e.target.value);
-    };
-
-    const handlePriorityChange = () => {
-        setPriority(!priority);
+    const onSubmit = (data) => {
+        console.log("Submitted Data:", data);
+        form.reset();
     };
 
     return (
         <div>
             <Title text="Ready to order? Let's go!" />
-            <form>
-                <div className="form-group">
-                    <label htmlFor="firstName">First Name</label>
-                    <Input
-                        id="firstName"
-                        type="text"
-                        value={firstName}
-                        onChange={handleFirstNameChange}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="phone">Phone number</label>
-                    <Input
-                        id="phone"
-                        type="tel"
-                        value={phone}
-                        onChange={handlePhoneChange}
-                        required
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="address">Address</label>
-                    <Input
-                        id="address"
-                        type="text"
-                        value={address}
-                        onChange={handleAddressChange}
-                        required
-                    />
-                </div>
-
-                <div className="checkbox-group">
-                    <div className="checkbox-wrapper">
+            <FormProvider {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                        <label htmlFor="firstName">First Name</label>
                         <Input
-                            id="priority"
-                            type="checkbox"
-                            checked={priority}
-                            onChange={handlePriorityChange}
+                            id="firstName"
+                            type="text"
+                            name="firstName"
+                            control={form.control}
+                            placeholder="Enter your first name"
                         />
-                        <label htmlFor="priority">Want to give your order priority?</label>
-                    </div>
-                </div>
 
-                <Button type="submit" className="order-btn" text="Order now for €12.00" />
-            </form>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="phone">Phone number</label>
+                        <Input
+                            id="phone"
+                            type="tel"
+                            name="phone"
+                            control={form.control}
+                            placeholder="Enter your phone number"
+
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="address">Address</label>
+                        <Input
+                            id="address"
+                            type="text"
+                            name="address"
+                            control={form.control}
+                            placeholder="Enter your address"
+                        />
+                    </div>
+
+                    <div className="checkbox-group">
+                        <div className="checkbox-wrapper">
+                            <Input
+                                id="priority"
+                                type="checkbox"
+                                name="priority"
+                                control={form.control}
+                            />
+                            <label htmlFor="priority">Want to give your order priority?</label>
+                        </div>
+                    </div>
+
+                    <Button type="submit" className="order-btn" text="Order now for €12.00" />
+                </form>
+            </FormProvider>
         </div>
     );
 };
